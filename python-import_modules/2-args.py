e98d55446a0638812de6ebc23f465d83296a4346
Elmir <siraclielmir9@gmail.com>
2026-03-13 10:34:19 +0000

    cjcneui

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-import sys
-
 if __name__ == "__main__":
-    argv = sys.argv
-    argc = len(argv) - 1  # script adı çıxılmalıdır
-    
-    if argc == 0:
-        print("0 arguments.")
-    elif argc == 1:
-        print("1 argument:")
+    import sys
+    i = len(sys.argv) - 1
+
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
     else:
-        print(f"{argc} arguments:")
-    
-    # arqumentləri çap et
-    for i in range(1, len(argv)):
-        print(f"{i}: {argv[i]}")
+        print("{} arguments:".format(i))
+
+    if i >= 1:
+        i = 0
+        for arg in sys.argv:
+            if i != 0:
+                print("{}: {}".format(i, arg))
+            i += 1
