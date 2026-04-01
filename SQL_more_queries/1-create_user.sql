a5d39b78a026d816451161754ec778905618e1cf
Elmir <siraclielmir9@gmail.com>
2026-04-01 08:35:01 +0000

    dkemkdwm

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,9 @@
--- Script that creates an user i
--- Query to create the user
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost'
+-- Create user if not exists
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Give all privileges
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Apply changes
+FLUSH PRIVILEGES;
